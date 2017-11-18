ce692875d396a5fab728a827260b33a24ba5fece
Andor Polgar <hngrhorace@gmail.com>
2017-11-18 20:12:33 +0100

    update data

--- src/template.js
+++ src/template.js
@@ -4,32 +4,44 @@ const renderPositions = cv => cv.positions.map(position => `
 	<li class="position">
 		<img class="position--image" src="${datauri('./src/assets/' + position.logo)}" />
 		<h3>${position.jobTitle}</h3>
-		<span>${position.company}</span>
-		<span>${position.startDate} - ${position.endDate}</span>
-		<span>${position.location}</span>
+		<span>${position.company}</span><br />
+		<small>${position.startDate} - ${position.endDate}
+		&bull; ${position.location}</small>
 		<p>${position.description}</p>
-		<p>${position.keywords.join(', ')}</p>
+		<p>Responsibilites: ${position.responsibilites.join(', ')}</p>
+		<p>Technolgies: ${position.technologies.join(', ')}</p>
 	</li>
 `).join('');
 
 const renderSchools = cv => cv.education.map(school => `
 	<li class="school">
+		<img class="school--logo" src="${datauri('./src/assets/' + school.logo)}" />
 		<h3>${school.school}</h3>
-		<span>${school.degree}</span>
-		<span>${school.fieldOfStudy}</span>
+		<span>${school.fieldOfStudy} ${school.degree}</span><br />
+		<small>${school.startDate} - ${school.endDate}
+		&bull; ${school.location}</small>
+		<p>Completed courses: ${school.completedCourses.join(', ')}</p>
+	</li>
+`).join('');
+
+const renderFaq = cv => cv.faq.map(qa => `
+	<li class="school">
+		<h3>${qa.q}</h3>
+		<span>${qa.a}</span>
 	</li>
 `).join('');
 
 const renderSocialLinks = cv => cv.socialLinks.map(link => `
 	<li>
-		<a href="${link.url}" target="_blank">${link.title}</a>
+		${link.title}: <a href="${link.url}" target="_blank">${link.url}</a>
 	</li>
 `).join('');
 
 module.exports = cv => `
-	<!DOCTYPE>
+	<!DOCTYPE html>
 	<html>
 		<head>
+			<meta charset="utf-8" />
 			<style>
 				@font-face {
 					font-family: Open Sans;
@@ -40,11 +52,13 @@ module.exports = cv => `
 		</head>
 		<body>
 			<img class="photo" src="${datauri('./src/assets/andor_cv_photo.jpg')}" />
-			<p>${cv.name}</p>
-			<p>Website: <a href="${cv.website}">${cv.website}</a></p>
-			<p>Location: ${cv.location}</p>
-			<p>Nationalities: ${cv.nationalities.join(', ')}</p>
-			<h2>Experience</h2>
+			<span>${cv.name}
+			<br />Website: <a href="${cv.website}">${cv.website}</a>
+			<br />Location: ${cv.location}
+			<br />Phone: ${cv.phone}
+			<br />Nationalities: ${cv.nationalities.join(', ')}</span>
+			<p>${cv.introduction}</p>
+			<h2>Professional experience</h2>
 			<ul>
 				${renderPositions(cv)}
 			</ul>
@@ -55,6 +69,10 @@ module.exports = cv => `
 			<h2>Social links</h2>
 			<ul>
 				${renderSocialLinks(cv)}
+			</ul>
+			<h2>Frequently Asked Questions</h2>
+			<ul>
+				${renderFaq(cv)}
 			</ul>
 		</body>
 	</html>
